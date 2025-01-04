f1cbf7191394fa35b56177287a8412cc36cf3a1a
Bin <bin.yoon@outlook.com>
2025-01-04 09:22:21 -0500

    Terrain render wip

--- src/main.cpp
+++ src/main.cpp
@@ -37,10 +37,68 @@ int main()
     Shader lightCubeShader("shaders/light_cube.vs", "shaders/light_cube.fs");
     Shader modelShader("shaders/model_loading.vs", "shaders/model_loading.fs");
     Shader skyboxShader("shaders/skybox.vs", "shaders/skybox.fs");
+    Shader terrainShader("shaders/terrain.vs", "shaders/terrain.fs");
 
     // load objects
     Model ourModel("assets/gm-bigcity/gm_bigcity.obj");
     Cube lampCube;
+
+    float vertices[] = {
+        // positions          // texture coords
+        0.5f, 0.0f, 0.5f, 1.0f, 1.0f,
+        0.5f, 0.0f, -0.5f, 1.0f, 0.0f,
+        -0.5f, 0.0f, -0.5f, 0.0f, 0.0f,
+        -0.5f, 0.0f, 0.5f, 0.0f, 1.0f};
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3};
+
+    // Setup VAO, VBO, EBO
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    // Texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Load the image
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("assets/rough-concrete-wall-texture.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    terrainShader.use();
+    terrainShader.setInt("texture1", 0);
 
     stbi_set_flip_vertically_on_load(false); // reset flip before loading skybox
     Skybox skybox;
@@ -67,8 +125,25 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 5000.0f);
         glm::mat4 view = camera.GetViewMatrix();
 
+        // Bind texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        terrainShader.use();
+        terrainShader.setMat4("projection", projection);
+        terrainShader.setMat4("view", view);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 100.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1000.0f));
+        terrainShader.setMat4("model", model);
+
+        // Render the plane
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         // draw the objects
-        ourModel.Draw(modelShader, projection, view, camera, animatedLightPos);
+        // ourModel.Draw(modelShader, projection, view, camera, animatedLightPos);
         lampCube.Draw(lightCubeShader, projection, view, camera, animatedLightPos);
         skybox.Draw(skyboxShader, projection, view, camera); // draw skybox as last
 
