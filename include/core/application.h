9e1da818fb765aada087ff3d75fd20b5cfaa1aa8
Bin <bin.yoon@outlook.com>
2025-08-27 17:55:40 -0400

    Add robot arm with valley

---
+++ include/core/application.h
@@ -0,0 +1,44 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "core/window.h"
+#include "render/shader.h"
+#include "render/shadowMap.h"
+#include "scene/camera.h"
+#include "scene/lights/directionalLight.h"
+#include "scene/lights/spotlight.h"
+#include "scene/model.h"
+#include "scene/robotArm.h"
+#include "scene/skybox.h"
+#include "scene/sphere.h"
+
+class Application
+{
+public:
+    Application();
+
+    void run();
+    void update();
+    void renderDepthPass();
+    void renderMainPass();
+
+private:
+    Window m_window;
+    Camera m_camera;
+    DirectionalLight m_sunLight;
+    Spotlight m_spotlight;
+    float m_sunSpeed{0.1f};
+    float m_spotlightGain{1.0f};
+
+    // GL-dependent resources are deferred
+    std::unique_ptr<RobotArm> m_robotArm;
+    std::unique_ptr<Model> m_valley;
+    std::unique_ptr<Skybox> m_skybox;
+    std::unique_ptr<ShadowMap> m_sunShadow;
+    std::unique_ptr<ShadowMap> m_spotShadow;
+    std::unique_ptr<Shader> m_modelShader;
+    std::unique_ptr<Shader> m_skyboxShader;
+    std::unique_ptr<Shader> m_depthShader;
+};
+
+#endif // APPLICATION_H
